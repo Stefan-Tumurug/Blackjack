3e44119df04b66aec961002950896358da06d11e
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-02-09 14:08:42 +0100

    Add Card domain model

---
+++ Blackjack.Core/Domain/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Core.Domain;
+
+public enum Suit
+{
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades
+}
+
+public enum Rank
+{
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 10,
+    Queen = 10,
+    King = 10,
+    Ace = 11
+}
+
+public sealed class Card
+{
+    public Suit Suit { get; }
+    public Rank Rank { get; }
+
+    public Card(Suit suit, Rank rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+
+    public int Value => (int)Rank;
+
+    public bool IsAce => Rank == Rank.Ace;
+
+    public override string ToString()
+    {
+        return $"{Rank} of {Suit}";
+    }
+}
