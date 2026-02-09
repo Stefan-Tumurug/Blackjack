c1a46248f6be7d29da0cd82f15a38ef244459cdb
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-02-09 14:33:28 +0100

    Fast sanity test to check Ace logic

--- Blackjack.Cli/Program.cs
+++ Blackjack.Cli/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blackjack.Core.Domain;
+
+// Hurtig sanity test: A, A, 9 skal give 21 (ikke 31).
+Hand hand = new Hand();
+hand.AddCard(new Card(Suit.Spades, Rank.Ace));
+hand.AddCard(new Card(Suit.Hearts, Rank.Ace));
+hand.AddCard(new Card(Suit.Clubs, Rank.Nine));
+
+Console.WriteLine(string.Join(", ", hand.Cards));
+Console.WriteLine($"Value: {hand.GetValue()} (Bust: {hand.IsBust})");
