8aa2b4eb4cf8cbde8c9c26c91aed8592083d0fcc
Stefan <147480126+Stefan-Tumurug@users.noreply.github.com>
2026-02-09 14:33:58 +0100

    Reverting CLI to placeholder until progress is made

--- Blackjack.Cli/Program.cs
+++ Blackjack.Cli/Program.cs
@@ -1,10 +1,1 @@
-﻿using Blackjack.Core.Domain;
-
-// Hurtig sanity test: A, A, 9 skal give 21 (ikke 31).
-Hand hand = new Hand();
-hand.AddCard(new Card(Suit.Spades, Rank.Ace));
-hand.AddCard(new Card(Suit.Hearts, Rank.Ace));
-hand.AddCard(new Card(Suit.Clubs, Rank.Nine));
-
-Console.WriteLine(string.Join(", ", hand.Cards));
-Console.WriteLine($"Value: {hand.GetValue()} (Bust: {hand.IsBust})");
+﻿Console.WriteLine("Blackjack CLI - work in progress");
